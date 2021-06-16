ca17fd436d48a941e2b57534eca018ae967fc5b9
infonizdev <vishnuinfoniz@gmail.com>
2021-06-16 16:50:43 +0530

    first commit

---
+++ spring-boot-server/src/test/java/com/infoniz/vas/VasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoniz.vas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
